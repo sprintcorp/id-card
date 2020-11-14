66a438d46dbde99cb8168b708607486c91c4efda
sprintcorp <sprintcorp7@gmail.com>
2020-11-14 13:49:45 -0800

    Update on Nav bar App icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>Adeyemi Demonstration Model</title>
+    <title>Adeyemi Demonstration Model Nursery and Primary School Akure ID Card Portal</title>
     <link rel="icon" type="image/x-icon" href="{{ URL::asset('images/logo.png')}}"/>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&amp;display=swap" rel="stylesheet">
